1f63481a59aee0465fea6f00d3de65cb3ada4126
refekt <refekt@gmail.com>
2021-09-05 19:41:38 -0400

    Package info updates

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="discord-surveys",
-    version="0.0.1",
+    name="discord_slash_surveys",
+    version="0.0.2",
     author="refekt",
     author_email="refekt@gmail.com",
     description="A survey object that utilizes discord.py and discord-interactions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/refekt/SlashSurveys",
+    url="https://github.com/refekt/discord-slash-surveys",
     project_urls={
-        "Bug Tracker": "https://github.com/refekt/SlashSurveys/issues",
+        "Bug Tracker": "https://github.com/refekt/discord-slash-surveys/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
