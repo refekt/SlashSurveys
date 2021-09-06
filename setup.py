acb5f031dbd8b0837b97905c9e53c37f5fe0bd1a
refekt <refekt@gmail.com>
2021-09-05 20:52:47 -0400

    Restructuring files

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+pkg_name = "discord-surveys"
+
 setuptools.setup(
-    name="discord_surveys",
-    version="0.0.3",
+    name=pkg_name,
+    version="1.0.0",
     author="refekt",
-    author_email="refekt@gmail.com",
     description="A survey object that utilizes discord.py and discord-interactions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,7 +21,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "discord-surveys"},
-    packages=setuptools.find_packages(where="discord-surveys"),
+    package_dir={"": pkg_name},
+    packages=setuptools.find_packages(where=pkg_name),
     python_requires=">=3.7",
 )
